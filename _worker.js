d5018e94744c09051714ddda47d23b5a1e7fc41f
hww-png <77440291+hww-png@users.noreply.github.com>
2022-06-17 22:58:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="wwhuang.tk";
+        url.hostname="www.wwhuang.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
